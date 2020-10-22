15b705d1a36fee0801ff65b89deaff87f2ce5b71
Laura de Andrade Castilho <54919378+LauraCastilho@users.noreply.github.com>
2020-10-22 00:33:23 -0300

    Add files via upload

---
+++ js/buscar.js
@@ -0,0 +1,7 @@
+function acharweb(){
+    var cidade = document.getElementsByName('cidade')
+    var estado = document.getElementsByName('estado')
+
+    window.location.assign("https://www.google.com.br/maps/search/"+ cidade.values + "," + estado.values + "+veterinarias/");
+
+}
